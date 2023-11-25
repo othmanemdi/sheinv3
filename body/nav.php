107ad8424aee4c000c2c5168c9b46a3a34f82e20
Othmane Mezgueldi <mo.atlantique@gmail.com>
2023-11-25 18:38:13 +0100

    Change color of navbar dark to primary

--- body/nav.php
+++ body/nav.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+<nav class="navbar navbar-expand-sm navbar-dark bg-primary">
     <div class="container-fluid">
         <a class="navbar-brand" href="#">Shein</a>
         <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
